3ebd5a5c55e0ae378d0263a0160feaec313e10ab
Bruz Marzolf <bmarzolf@systemsbiology.org>
2010-09-29 13:40:28 -0700

    Allow for different annotations for Exiqon slides based on slide number

--- scripts/vera-sam-pipeline.rb
+++ scripts/vera-sam-pipeline.rb
@@ -3,6 +3,7 @@ require 'json'
 require 'fileutils'
 
 ARRAY_SHARE = "/net/arrays"
+GAL_FOLDER = ARRAY_SHARE + "/Legacy/ExiqonGAL"
 
 #raise "vera-sam-pipeline.rb expects a single JSON string argument" unless ARGV.size == 1
 
@@ -13,6 +14,23 @@ begin
   microarrays = JSON.parse(json_string)
 rescue Exception => e
   raise "Unable to parse JSON: #{e}"
+end
+
+# load in the slide-to-annotation mappings
+begin
+  yaml = YAML.load(File.new(GAL_FOLDER + "/slide-to-annotations.yml"))
+
+  mappings = Array.new
+  yaml.each do |slide_range, gal_file|
+    slides = slide_range.split(/-/)
+    mappings << {
+      :start_slide => slides[0].to_i,
+      :end_slide => slides[1].to_i,
+      :gal_file => gal_file
+    }
+  end
+rescue Exception => e
+  raise "Unable to load the slide-to-annotation mapping file: #{e}"
 end
 
 # generate a script and then run the whole job as one command
@@ -27,10 +45,20 @@ microarrays.each do |microarray|
     raise "Path #{file_path} must lie under #{ARRAY_SHARE}"
   end
 
-  rep_file_name = File.basename(file_path) + ".rep"
+  # annotations
+  file_base = File.basename(file_path, ".csv")
+  slide_number = file_base.to_i
+  gal_file = nil
+  mappings.each do |mapping|
+    if slide_number >= mapping[:start_slide] && slide_number <= mapping[:end_slide]
+      gal_file = GAL_FOLDER + "/" + mapping[:gal_file]
+    end
+  end
+  script << "/tools/bin/perl /net/arrays/Legacy/Core/bin/gal2map.pl --gal #{gal_file} --map #{file_base}.map\n"
 
   # preprocess
-  script << "/tools/java/jdk/bin/java -Xmx512M -jar /net/arrays/bin/preprocess.jar -q #{file_path} -m /net/arrays/Slide_Templates/exiqon_208202-A_lot31021_hsa,mmu,rno-and-related-vira_from_mb120,miRPlus.map -o #{rep_file_name} -i -n median\n"
+  rep_file_name = file_base + ".rep"
+  script << "/tools/java/jdk/bin/java -Xmx512M -jar /net/arrays/bin/preprocess.jar -q #{file_path} -m #{file_base}.map -o #{rep_file_name} -i -n median\n"
 
   # ft
   ft = File.open("#{condition}.ft", "w")
